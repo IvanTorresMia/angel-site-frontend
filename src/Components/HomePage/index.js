539dc5cc1f2305d3946e746a7a2c5db2dcd64bb8
Ivan Torres <ivantorresmia27@gmail.com>
2022-07-09 16:40:29 -0700

    started comp 4

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -3,6 +3,7 @@ import Header from '../Header';
 import Home from '../Home';
 import Comp2 from '../HomePageComponents/Comp2';
 import Comp3 from '../HomePageComponents/Comp3';
+import Comp4 from '../HomePageComponents/Comp4';
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
     <Header />
     <Comp2 />
     <Comp3 />
+    <Comp4 />
     </>
   )
 }
---
+++ src/Components/HomePageComponents/Comp4.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "react-bootstrap/esm/Button";
+
+const Comp4 = () => {
+  return (
+    <div>
+      <h2>OUR MISSION</h2>
+      <div>
+        <p>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Latin literature from 45 BC, making
+          it over 2000 years old.
+        </p>
+        <span>angeltorres@gmail.com</span><br/>
+        <Button>Contact Me</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Comp4;
